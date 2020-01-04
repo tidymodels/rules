f0f17c1223cc92733695fafecc0b2d9eebd9601a
topepo <mxkuhn@gmail.com>
2020-01-04 13:41:09 -0500

    removed parsnip functions

--- R/0_imports.R
+++ R/0_imports.R
@@ -2,7 +2,9 @@
 #' @importFrom rlang enquos expr
 #' @importFrom purrr map_dfr
 #' @importFrom tibble is_tibble as_tibble tibble
-#' @importFrom parsnip set_new_model multi_predict
+#' @importFrom parsnip set_new_model multi_predict update_dot_check show_fit
+#' @importFrom parsnip new_model_spec is_varying null_value update_main_parameters
+#' @importFrom parsnip check_final_param
 #' @importFrom stats predict model.frame model.response setNames
 #' @importFrom dials new_quant_param
 #' @importFrom tidyr nest
